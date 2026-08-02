bddaa808ca6ade1b533758d472d8807ed567f28c
Angel Eduardo Hincho Jove <82984150+ahincho@users.noreply.github.com>
2026-08-01 19:07:20 -0500

    fix(cleanup): remove deprecated modules/security (Sprint 7-03 + 7-04 prep)

--- modules/security/variables.tf
+++
@@ -1,9 +0,0 @@
-###############################################################################
-# Variables de modules/security (DEPRECATED meta-module)
-#
-# Ver modules/oidc-github, modules/kms, modules/security-groups para los
-# sub-modulos activos. Los callers deben usar esos directamente.
-#
-# Este archivo queda para evitar breakage de imports externos. Sera eliminado
-# en Sprint 2 (cleanup).
-###############################################################################
